a688c4cc17dbc0f86c28b5b1f9c6f181a5c89a05
grdRaphael <drebin235@gmail.com>
2026-08-04 17:25:28 +0400

    Enforce unique and normalized email on User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose"
+import uniqueValidator from "mongoose-unique-validator"
+
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true}, 
+    email: {type: String, required: true, unique: true, lowercase: true, trim: true}, 
     password: {type: String, required: true}
 })
+
+userSchema.plugin(uniqueValidator)
 
 export default mongoose.model("User", userSchema)
